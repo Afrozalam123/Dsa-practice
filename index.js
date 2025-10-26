c9d134922b31264741d071e105b1151f79dd22c1
Afrozalam123 <alamafroz70400@gmail.com>
2025-10-26 19:25:24 +0530

    please updated my new code

---
+++ index.js
@@ -0,0 +1,15 @@
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+
+
+// function search(nums,target){
+//   for(let i=0;i<nums.length;i++){
+//     if(nums[i]==target){
+//       return target
+//     }
+//   }
+// }
+// let nums=[-1,0,3,5,9,12]
+// console.log(search(nums))
+
+
---
+++ practice.js
@@ -0,0 +1,27 @@
+// / let num=[0,1,3]
+// output:2
+
+// function missingnumber(nums){
+//   let n=nums.length;
+//   let total=(n*(n+1))/2
+//   let sum=0;
+//   for(let i=0;i<nums.length;i++){
+//    sum+=nums[i]
+//   }
+//   return total-sum
+// }
+// let nums=[0,1,3]
+// console.log(missingnumber(nums))
+
+// input nums=[0,1,0,3,12]
+// output:[1,3,12,0,0]
+
+
+// function movezeros(nums){
+//   let j=0;
+//   for(let i=0;i<nums.length;i++){
+//     if(nums[j]=nums[i]){
+//         j++
+//     }
+     
+//   }

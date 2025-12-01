2029122ad3b7d78907246b9d613ec4c6b45f002d
Afrozalam123 <alamafroz70400@gmail.com>
2025-12-01 23:16:19 +0530

    please  update my code

---
+++ Array/ArrayPractice/ray.js
@@ -0,0 +1,21 @@
+// Q1. Sum of All Elements
+
+// Ek integer array diya gaya hai.
+// Task: Array ke sabhi elements ka sum nikalna hai.
+
+// Example:
+// Input: arr = [1, 2, 3, 4]
+// Output: 10
+
+
+//  function add(arr){
+//   let sum=0;
+//   for(let i=0;i<arr.length;i++){
+//     sum+=arr[i]
+//   }
+//   return sum;
+//  }
+//  let arr=[1,2,3,4]
+//  console.log(add(arr))
+
+
